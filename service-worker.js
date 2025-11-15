3095c6bdb40764050548b44739ab7b7ba990c839
Md Zafar <150018673+mdzafar864@users.noreply.github.com>
2025-11-16 03:03:29 +0530

    Refactor service worker for installation and activation
    Removed cache installation logic and added skipWaiting and clients.claim.

--- service-worker.js
+++ service-worker.js
@@ -1,19 +1,7 @@
-self.addEventListener("install", e => {
-  e.waitUntil(
-    caches.open("beu-cache").then(cache => {
-      return cache.addAll([
-        "./",
-        "./index.html",
-        "./manifest.json",
-        "./icon-192.png",
-        "./icon-512.png"
-      ]);
-    })
-  );
+self.addEventListener("install", () => {
+  self.skipWaiting();
 });
 
-self.addEventListener("fetch", e => {
-  e.respondWith(
-    caches.match(e.request).then(res => res || fetch(e.request))
-  );
+self.addEventListener("activate", () => {
+  clients.claim();
 });
