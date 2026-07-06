422bb9e15ffca6290efc88b6d28ecadfecd42630
Md Zafar <150018673+mdzafar864@users.noreply.github.com>
2026-07-06 12:05:45 +0530

    Update cache name to v18

--- service-worker.js
+++ service-worker.js
@@ -2,7 +2,7 @@
 //  UNIVERSAL PWA SW (GitHub + Netlify)
 // =============================
 
-const CACHE_NAME = "beu-pwa-v17";
+const CACHE_NAME = "beu-pwa-v18";
 
 // 🔥 AUTO detect base path
 const BASE = self.location.hostname.includes("github.io")
