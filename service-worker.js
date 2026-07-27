39235953f81d12bd479374f2749116f4c25d74ac
Md Zafar <150018673+mdzafar864@users.noreply.github.com>
2026-07-28 00:48:40 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -2,7 +2,7 @@
 //  UNIVERSAL PWA SW (GitHub + Netlify)
 // =============================
 
-const CACHE_NAME = "beu-pwa-v29";
+const CACHE_NAME = "beu-pwa-v30";
 
 // 🔥 AUTO detect base path
 const BASE = self.location.hostname.includes("github.io")
