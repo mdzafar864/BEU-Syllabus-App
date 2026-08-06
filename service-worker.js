4500945e0238292de491930ebd51d1e302e4b54e
Md Zafar <150018673+mdzafar864@users.noreply.github.com>
2026-08-07 04:47:06 +0530

    Update cache name from v32 to v33

--- service-worker.js
+++ service-worker.js
@@ -2,7 +2,7 @@
 //  UNIVERSAL PWA SW (GitHub + Netlify)
 // =============================
 
-const CACHE_NAME = "beu-pwa-v32";
+const CACHE_NAME = "beu-pwa-v33";
 
 // 🔥 AUTO detect base path
 const BASE = self.location.hostname.includes("github.io")
