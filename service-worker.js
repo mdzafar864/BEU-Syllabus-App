962439e0e972d4f33b82e5e2d8b15992f16c200f
Md Zafar <150018673+mdzafar864@users.noreply.github.com>
2026-08-16 23:45:17 +0530

    Increment cache version to v39
    Updated cache name to version 39.

--- service-worker.js
+++ service-worker.js
@@ -2,7 +2,7 @@
 //  UNIVERSAL PWA SW (GitHub + Netlify)
 // =============================
 
-const CACHE_NAME = "beu-pwa-v38";
+const CACHE_NAME = "beu-pwa-v39";
 
 // 🔥 AUTO detect base path
 const BASE = self.location.hostname.includes("github.io")
